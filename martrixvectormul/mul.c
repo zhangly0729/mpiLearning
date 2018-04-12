28262ea260a45079cd19b07f55562328a166f656
wusihai <wsh_18980796471@sina.com>
2018-04-12 02:00:47 -0700

    It can divide the Martrix

--- martrixvectormul/mul.c
+++ martrixvectormul/mul.c
@@ -10,7 +10,7 @@
 #include<stdlib.h>
 int main(int argc,char**argv)
 {
-	int i,j,k,L;
+	int i,j,k,L,D;
 	int rank,size;
 	
 	MPI_Init(&argc,&argv);
@@ -18,12 +18,14 @@ int main(int argc,char**argv)
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
 	//paramter 
-	L=200;
-	float *a=NULL,*b=NULL,c=0;
+	L=20;D=L/size;
+	float *a=NULL,*b=NULL,c[D];
 	float *A=NULL,*C=NULL;
-	a=(float*)malloc(sizeof(float)*L);
+	a=(float*)malloc(sizeof(float)*L*D);
 	b=(float*)malloc(sizeof(float)*L);
 
+	for(i=0;i<D;i++)
+		c[i]=0;
 	//initial the martrix & vector
 	if(rank==0)
 	{
@@ -41,17 +43,17 @@ int main(int argc,char**argv)
 	//BroadCast the vector b to child process
 	MPI_Bcast(b,L,MPI_FLOAT,0,MPI_COMM_WORLD);
 	//Scatter the martrix A to slave process  
-	MPI_Scatter(A,L,MPI_FLOAT,a,L,MPI_FLOAT,0,MPI_COMM_WORLD);
+	MPI_Scatter(A,D*L,MPI_FLOAT,a,D*L,MPI_FLOAT,0,MPI_COMM_WORLD);
 
 	//caculate
-	for(i=0;i<L;i++)
-	{
-		c+=a[i]*b[i];
-	}
+	for(j=0;j<D;j++)
+		for(i=0;i<L;i++)
+		{
+			c[j]+=a[i]*b[i];
+		}
 	
-
 	//Gather the value c to result vector C in master process
-	MPI_Gather(&c,1,MPI_FLOAT,C,1,MPI_FLOAT,0,MPI_COMM_WORLD);
+	MPI_Gather(&c,D,MPI_FLOAT,C,D,MPI_FLOAT,0,MPI_COMM_WORLD);
 
 	//print the result
 	if(rank==0)
