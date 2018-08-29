a275dc6f8d32e4b33bb7f68fb700d1442af9a04c
wusihai <wsh_18980796471@sina.com>
2018-08-29 14:19:24 +0800

    modify cufft kernel.cu

--- kernel.cu
+++ kernel.cu
@@ -19,9 +19,10 @@ __global__ void normalizing(cufftComplex* data, int data_len)
 }
 int main()
 {
+	cudaSetDevice(1);
 	//uint64_t
-	uint64_t Nt =1024LL*512*1024*1.2/8;
-	uint64_t datasize = 1024LL * 1024 * 1024 * 1.2 / 8 * 8;
+	uint64_t Nt =1024LL*1024*200;
+	uint64_t datasize =1024LL * 1024 * 200*8;
 	const int BATCH = 1;
 	//BATCH用于批量处理一批一维数据，当BATCH=2时
 	//则将0-1024，1024-2048作为两个一维信号做FFT处理变换
@@ -29,6 +30,8 @@ int main()
 	//主机内存申请及初始化--主机锁页内存
 	Check(cudaMallocHost((void**)&host_in, datasize ));
 	Check(cudaMallocHost((void**)&host_out, datasize));
+//	host_in=(cufftComplex*)malloc(datasize);
+//	host_out=(cufftComplex*)malloc(datasize);
 	//Nt = Nt / 8;
 	for (int i = 0; i < Nt; i++)
 	{
@@ -36,23 +39,28 @@ int main()
 		host_in[i].y = i + 1;
 	}
 	//设备内存申请
+	size_t freeMem, totalMem;
+	cudaMemGetInfo(&freeMem, &totalMem);
 	Check(cudaMalloc((void**)&device_in, Nt * sizeof(cufftComplex)));
-	Check(cudaMalloc((void**)&device_out, Nt * sizeof(cufftComplex)));
+	cudaMemGetInfo(&freeMem, &totalMem);
+	//Check(cudaMalloc((void**)&device_out, Nt * sizeof(cufftComplex)));
+//	cudaMemGetInfo(&freeMem, &totalMem);
 	//数据传输--H2D
 	Check(cudaMemcpy(device_in, host_in, Nt * sizeof(cufftComplex), cudaMemcpyHostToDevice));
- 
+
+
 	//创建cufft句柄
 	cufftHandle cufftForwrdHandle, cufftInverseHandle;
 	cufftResult_t cufftstate;
 	cufftstate =cufftPlan1d(&cufftForwrdHandle, Nt, CUFFT_C2C, BATCH);
 	if (cufftstate)	cout << "cufft plan create failed!" << endl;
 	cufftstate=cufftPlan1d(&cufftInverseHandle, Nt, CUFFT_C2C, BATCH);
- 
+
 	//执行fft正变换
-	cufftExecC2C(cufftForwrdHandle, device_in, device_out, CUFFT_FORWARD);
+	cufftExecC2C(cufftForwrdHandle, device_in, device_in, CUFFT_FORWARD);
  
 	//数据传输--D2H
-	Check(cudaMemcpy(host_out, device_out, Nt * sizeof(cufftComplex), cudaMemcpyDeviceToHost));
+	Check(cudaMemcpy(host_in, device_in, Nt * sizeof(cufftComplex), cudaMemcpyDeviceToHost));
  
 	//设置输出精度--正变换结果输出
 	cout << "正变换结果:" << endl;
@@ -63,7 +71,7 @@ int main()
 	}
  
 	//执行fft反变换
-	cufftExecC2C(cufftInverseHandle, device_out, device_in, CUFFT_INVERSE);
+	cufftExecC2C(cufftInverseHandle, device_in, device_in, CUFFT_INVERSE);
  
 	//IFFT结果是真值的N倍，因此要做/N处理
 	dim3 grid(Nt / 128);
@@ -72,14 +80,14 @@ int main()
  
 	//数据传输--D2H
 	Check(cudaMemcpy(host_in, device_in, Nt * sizeof(cufftComplex), cudaMemcpyDeviceToHost));
- 
-	//设置输出精度--反变换结果输出
-	cout << "反变换结果:" << endl;
-	cout.setf(20);
-	for (int i = 0; i < Nt; i++)
-	{
-		//cout << host_in[i].x << "+j*" << host_in[i].y << endl;
-	}
-	//cin.get();
+ //
+	////设置输出精度--反变换结果输出
+	//cout << "反变换结果:" << endl;
+	//cout.setf(20);
+	//for (int i = 0; i < Nt; i++)
+	//{
+	//	//cout << host_in[i].x << "+j*" << host_in[i].y << endl;
+	//}
+	////cin.get();
 	return 0;
 }
